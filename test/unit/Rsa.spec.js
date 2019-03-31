e9dedee5cdf05c9b1cd536e345335e3dced6f212
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2019-03-31 20:11:34 +0300

    wip

--- test/unit/Rsa.spec.js
+++ test/unit/Rsa.spec.js
@@ -1,10 +1,14 @@
 import {Rsa} from '../../module/Rsa'
+var expect = require('chai').expect
 
 describe('Rsa',()=>{
     let stub = new Rsa()
+    console.log('publickey',stub.RsaModule.exportKey('public'))
+    console.log('privateKey',stub.RsaModule.exportKey())
+
     describe('Encrypt',()=>{
         it('encrypt data',()=>{
-
+            expect(stub.decrypt(stub.encrypt('hello world'))).to.be.eql('hello world')
         })
     })
 })
