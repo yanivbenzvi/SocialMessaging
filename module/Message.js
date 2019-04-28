239a29b56f7d6bd3f0d11bb93f470c5a601fff89
Pavel Shvarchov <shvarpa@gmail.com>
2019-04-28 14:52:09 +0300

    merged message classes

--- module/Message.js
+++ module/Message.js
@@ -5,13 +5,79 @@ export class Message {
      * @param {Object} message
      */
     constructor(message = {}) {
-        const {to, from, timeStamp, textMessage} = message
+        const { to, from, time, body } = message
 
-        this.to          = to
-        this.from        = from
-        this.timeStamp   = timeStamp
-        this.textMessage = textMessage
-        this.readed      = false
-        this.mKey        = md5(to + from + timeStamp)
+        this.to = to
+        this.from = from
+        this.time = time
+        this.body = body
+        this.readed = false
+        this.mKey = md5(to + from + time)
+    }
+
+    from_object(obj) {
+        const { to, from, time, body } = obj;
+        this.to = to;
+        this.from = from;
+        this.time = time;
+        this.body = body;
+        this.readed = false;
+        this.mKey = md5(to + from + body + time);
+        return true;
+    }
+
+    to_object() {
+        return {
+            'to': this.to,
+            'from': this.from,
+            'time': this.time,
+            'body': this.body,
+        };
+    }
+
+    from_JSON(message) {
+        message = JSON.parse(message);
+        return this.from_object(message);
+    }
+
+    to_JSON() {
+        return JSON.stringify(this.to_object());
+    }
+
+    static _attributes_order() {
+        return ['to', 'from', 'time', 'body']
+    }
+
+    static _terminal() {
+        return "/~?"
+    }
+
+    from_string(str) {
+        let keys = Message._attributes_order();
+        let values = str.split(Message._terminal());
+        let message = {}
+        if (keys.length != values.length)
+            return false;
+        for (let i = 0; i < keys.length; i++) {
+            message[keys[i]] = values[i];
+        }
+        return this.from_object(message);
+    }
+
+    is_valid() {
+        for (let attr of Message._attributes_order()) {
+            if (!(attr in this))
+                return false;
+        }
+        return true;
+    }
+
+    to_string() {
+        if (!this.is_valid()) return null;
+        let str_arr = []
+        for (let attr of Message._attributes_order()) {
+            str_arr.push(this[attr])
+        }
+        return str_arr.join(Message._terminal());
     }
 }
--- test/unit/message.spec.js
+++ test/unit/message.spec.js
@@ -6,8 +6,68 @@ describe('Message', () => {
     it('has the follow attribute', () => {
         const message      = new Message()
         const stub = Object.keys(message)
-        const attribute = ['to', 'from', 'timeStamp', 'textMessage', 'readed', 'mKey']
+        const attribute = ['to', 'from', 'time', 'body', 'readed', 'mKey']
 
         expect(stub).to.be.eql(attribute)
     })
+    describe('#Object', () => {
+        let message = new Message();
+        let input = {
+            to: "b",
+            from : "a",
+            time : "00:00:00",
+            body: "this is a message",
+        };
+        let res = message.from_object(input);
+        describe('#from_object', () => {
+            it("should say succeded in operation", () => {
+                expect(res).to.be.equal(true);
+            });
+        });
+        describe("#to_object", () => {
+            it("message.to_JSON() should mean the same as the input JSON", () => {
+                let object_export = message.to_object();
+                expect(object_export).to.be.deep.equal(input);
+            })
+        })
+    });
+    describe('#JSON', () => {
+        let message = new Message();
+        let input = `{
+            "to": "b",
+            "from" : "a",
+            "time" : "00:00:00",
+            "body": "this is a message"
+        }`;
+        let res = message.from_JSON(input);
+        describe('#from_JSON', () => {
+            it("should say succeded in operation", () => {
+                expect(res).to.be.equal(true);
+            });
+        });
+        describe("#to_JSON", () => {
+            it("message.to_JSON() should mean the same as the input JSON", () => {
+                let json_export = message.to_JSON();
+                expect(JSON.parse(json_export)).to.be.deep.equal(JSON.parse(input));
+            })
+        })
+    });
+
+    describe('#String', () => {
+        let message = new Message();
+        let terminal = Message._terminal();
+        let input = ["b","a","00:00:00","this is a message"].join(terminal);        
+        let res = message.from_string(input);
+        describe('#from_string', () => {
+            it("should say succeded in operation", () => {
+                expect(res).to.be.equal(true);
+            });
+        });
+        describe("#to_string", () => {
+            it("message.to_string() should mean the same as the input string", () => {
+                let string_export = message.to_string();
+                expect(string_export).to.be.deep.equal(input);
+            })
+        })
+    });
 })
